b0d2f36a444e8b2c305932ee692dde289fda4bae
Rivalser <97056846+Rivalser@users.noreply.github.com>
2023-09-29 22:30:47 +0200

    Órai hiba javítva

--- course/src/main/java/com/attrecto/academy/java/course/AttrectoAcademyCourseAppApplication.java
+++ course/src/main/java/com/attrecto/academy/java/course/AttrectoAcademyCourseAppApplication.java
@@ -2,13 +2,16 @@ package com.attrecto.academy.java.course;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @SpringBootApplication
 public class AttrectoAcademyCourseAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AttrectoAcademyCourseAppApplication.class, args);
-		//Teszt, hogy ez is megy-e
+		
 	}
 
 }
---
+++ course/src/main/java/com/attrecto/academy/java/course/controller/Controller.java
@@ -0,0 +1,13 @@
+package com.attrecto.academy.java.course.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@RestController
+@RequestMapping("/api/courses")
+@Tag(name = "Course API")
+public class Controller {
+
+}
